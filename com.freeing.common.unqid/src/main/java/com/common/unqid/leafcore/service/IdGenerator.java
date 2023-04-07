f8598634d00377d6156e4c98b8c150ce4371193b
yanggy <ilike.yanggy@gmail.com>
2023-04-07 22:07:44 +0800

    唯一性id服务：segment 解决方案

---
+++ com.freeing.common.unqid/src/main/java/com/common/unqid/leafcore/service/IdGenerator.java
@@ -0,0 +1,20 @@
+package com.common.unqid.leafcore.service;
+
+import com.common.unqid.leafcore.IdManager;
+import com.common.unqid.leafcore.segment.model.ID;
+import com.common.unqid.leafcore.segment.model.entity.LeafAlloc;
+
+/**
+ * ID 生成接口
+ */
+public interface IdGenerator {
+    /**
+     * 获取 ID
+     *
+     * @param key 业务区分标识
+     * @return Result
+     */
+    ID get(String key);
+
+    IdManager getIdManager();
+}
---
+++ com.freeing.common.unqid/src/main/java/com/common/unqid/server/exception/LeafServerException.java
@@ -0,0 +1,10 @@
+package com.common.unqid.server.exception;
+
+/**
+ * @author yanggy
+ */
+public class LeafServerException extends RuntimeException {
+    public LeafServerException(String msg) {
+        super(msg);
+    }
+}

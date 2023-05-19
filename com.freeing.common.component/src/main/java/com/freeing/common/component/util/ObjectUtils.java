71626ce2ebfa0bcfceec150c76d56ffe03172306
yanggy <ilike.yanggy@gmail.com>
2023-05-19 22:54:24 +0800

    fix: trim 工具增强，处理父类的属性

--- com.freeing.common.component/src/main/java/com/freeing/common/component/util/ObjectUtils.java
+++ com.freeing.common.component/src/main/java/com/freeing/common/component/util/ObjectUtils.java
@@ -8,6 +8,8 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.HashSet;
 
 /**
  * @author yanggy
@@ -20,45 +22,57 @@ public class ObjectUtils extends org.apache.commons.lang3.ObjectUtils {
      * 为对象中有 @Trim 注解的 String 属性去两边空格
      * {@link Trim}
      *
+     * @param ignored 忽略属性
      * @param object 对象
      */
-    public static void trimByAnno(Object object) {
+    public static void trimByAnnotation(Object object, String... ignored) {
         if (object == null) {
             return;
         }
+        HashSet<String> ignoredSet = new HashSet<>();
+        if (ignored != null) {
+            ignoredSet.addAll(Arrays.asList(ignored));
+        }
         Class<?> clazz = object.getClass();
-        boolean TrimPresentType = clazz.isAnnotationPresent(Trim.class);
-        Field[] fields = clazz.getDeclaredFields();
-        for (Field field : fields) {
-            field.setAccessible(true);
-            // 忽略 final 和 static 属性
-            if (Modifier.isFinal(field.getModifiers()) || Modifier.isStatic(field.getModifiers())) {
-                continue;
-            }
-            // 类和属性都不存在 Trim 注解则跳过
-            if (!field.isAnnotationPresent(Trim.class) && !TrimPresentType) {
-                continue;
-            }
-            try {
-                // 获取属性值并判断是否需要 trim
-                Object attrValue = field.get(object);
-                if (attrValue == null || attrValue.equals("")) {
+        while (clazz != null){
+            boolean TrimPresentType = clazz.isAnnotationPresent(Trim.class);
+            Field[] fields = clazz.getDeclaredFields();
+            for (Field field : fields) {
+                field.setAccessible(true);
+                // 忽略 final 和 static 属性
+                if (Modifier.isFinal(field.getModifiers()) || Modifier.isStatic(field.getModifiers())) {
                     continue;
                 }
-                if (attrValue instanceof String) {
-                    String fieldName = field.getName();
-                    char[] chars = fieldName.toCharArray();
-                    chars[0] = (char) (chars[0] - 32);
-                    // 为什么采用调用 setter 方法的方式而不是调用 Field#set：是为保证封装的完整性，setter 方法可以存在其他的操作
-                    // 如：拼接前缀，后缀等等
-                    String setMethodName = "set" + String.valueOf(chars);
-                    Method method = clazz.getMethod(setMethodName, String.class);
-                    method.invoke(object, attrValue.toString().trim());
+                // 类和属性都不存在 Trim 注解则跳过
+                if (!field.isAnnotationPresent(Trim.class) && !TrimPresentType) {
+                    continue;
                 }
-            } catch (IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
-                log.warn("class: {}", object.getClass().getTypeName(), e);
+                if (ignoredSet.contains(field.getName())) {
+                    continue;
+                }
+                try {
+                    // 获取属性值并判断是否需要 trim
+                    Object attrValue = field.get(object);
+                    if (attrValue == null || attrValue.equals("")) {
+                        continue;
+                    }
+                    if (attrValue instanceof String) {
+                        String fieldName = field.getName();
+                        char[] chars = fieldName.toCharArray();
+                        chars[0] = (char) (chars[0] - 32);
+                        // 为什么采用调用 setter 方法的方式而不是调用 Field#set：是为保证封装的完整性，setter 方法可以存在其他的操作
+                        // 如：拼接前缀，后缀等等
+                        String setMethodName = "set" + String.valueOf(chars);
+                        Method method = clazz.getMethod(setMethodName, String.class);
+                        method.invoke(object, attrValue.toString().trim());
+                    }
+                } catch (IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+                    log.warn("class: {}", object.getClass().getTypeName(), e);
+                }
+                field.setAccessible(false);
             }
-            field.setAccessible(false);
+            // 处理父类
+            clazz = clazz.getSuperclass();
         }
     }
 }

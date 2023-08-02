d3553a6fdcc1a33f570da10b5876c1e22e1411c9
yanggy <ilike.yanggy@gmail.com>
2023-08-02 20:31:23 +0800

    update log

--- com.freeing.common.log/src/main/java/com/freeing/common/log/listener/LogListener.java
+++ com.freeing.common.log/src/main/java/com/freeing/common/log/listener/LogListener.java
@@ -4,30 +4,21 @@ import com.freeing.common.log.domain.OperationLog;
 import com.freeing.common.log.event.LogEvent;
 import org.springframework.context.event.EventListener;
 
-import java.util.function.Consumer;
-
 /**
  * 操作日志的监听器，异步处理日志
- * ps: 外部引用时需要配置 consumer 并且交给 spring 管理
  * 推荐实现 ApplicationListener<LogEvent> 接口，或者继承 BaseLogListener
  * @see BaseLogListener
  *
  * @author yanggy
  */
 @Deprecated
-public class LogListener {
-    /**
-     * 消费者函数，日记记录的工作交给lambda表达式来完成
-     */
-    private final Consumer<OperationLog> consumer;
-
-    public LogListener(Consumer<OperationLog> consumer) {
-        this.consumer = consumer;
-    }
+public abstract class LogListener {
 
     @EventListener(LogEvent.class)
     public void saveLog(LogEvent event) {
-        OperationLog optLog = (OperationLog) event.getSource();
-        consumer.accept(optLog);
+        OperationLog operationLog = (OperationLog) event.getSource();
+        this.apply(operationLog);
     }
+
+    protected abstract void apply(OperationLog operationLog);
 }
